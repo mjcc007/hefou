b773876a0f9e3c3e41b13c8f4407acd9c0dde4ac
mjcc <chengcheng001@outlook.com>
2018-07-15 22:59:39 +0800

    添加头部组件并进行相关修改

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,6 +11,18 @@ export default {
 </script>
 
 <style>
+*{
+	margin: 0;
+	padding: 0;
+}
+body{
+	background-color:#f3f3f3;
+}
+
+a{
+	color: #2f889a;
+	text-decoration: none;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
